05f62580650aff1c84db0f23a3adda8c12a41373
Lai Zenan <z@laizn.com>
2023-11-27 00:33:08 +0800

    feat: add default value for isOccupie

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  await app.listen(
+    3000,
+    process.env.NODE_ENV === 'production' ? '0.0.0.0' : '127.0.0.1',
+  );
 }
 bootstrap();
