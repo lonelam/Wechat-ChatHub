b21ada3b5b5c13640edc71d16ecc16c6806c4827
laizenan <laizenan@gmail.com>
2023-11-29 15:34:59 +0000

    fix: path in tar gz

--- scripts/remote_backup.sh
+++ scripts/remote_backup.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-# Replace [volume_name] with the name of your Docker volume
-VOLUME_NAME=chathub_db_data
+VOLUME_NAME="chathub_db_data"
+ARCHIVE_NAME="/tmp/chathub_db_data.tar.gz"
 
 # Finding the volume mount point
-VOLUME_MOUNTPOINT=$(docker volume inspect $VOLUME_NAME | grep Mountpoint | cut -d '"' -f 4)
+VOLUME_MOUNTPOINT=$(docker volume inspect "$VOLUME_NAME" -f '{{ .Mountpoint }}')
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to find the mount point of the volume."
+    exit 1
+fi
 
-# Replace [archive_name] with your desired archive name
-ARCHIVE_NAME=chathub_db_data.tar.gz
+# Change to the directory containing the volume data
+cd "$VOLUME_MOUNTPOINT" || exit
 
 # Creating a compressed archive of the volume
-tar czvf /tmp/$ARCHIVE_NAME $VOLUME_MOUNTPOINT
+tar czvf "$ARCHIVE_NAME" . || exit
 
 echo "Archive created: $ARCHIVE_NAME"
+
